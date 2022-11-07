cd1f089438230e95562a03fa1726780535936710
YusufRoshan <112139503+YusufRoshan@users.noreply.github.com>
2022-11-07 21:37:35 +0000

    #1

--- q1.cpp
+++ q1.cpp
@@ -1,27 +1,14 @@
 #include <iostream>
-#include <iomanip>
 using namespace std;
 
-//********************
-// Declare Function Prototypes
-//********************
-
-int main()
+void swapTwoValues(int n1,int n2)
 {
-	int num1, num2;
-	float fnum1, fnum2;
-	getinput(num1, num2);
-	cout << " Before Swap" << setw(5) << num1 << setw(5) << num2 << setw(5) << endl;
-	swapTwoValues(num1, num2);
-	cout << " After Swap" << setw(5) << num1 << setw(5) << num2 << setw(5) << endl;
-
-	// -- Call overloaded function with float parameters
-	getinput(fnum1, fnum2);
-	cout << setw(5) << fnum1 << setw(5) << fnum2 << setw(5) << endl;
-	swapTwoValues(fnum1, fnum2);
-	cout << setw(5) << fnum1 << setw(5) << fnum2 << setw(5) << endl;
-}
-
-// ******************************
-// Implement all your functions here
-// ******************************
+int temp;
+cout << "Enter Int n1 and n2:";
+cin>> n1 >> n2;
+cout<<"Before swap int n1 = "<<n1<<" int n2 = "<<n2<<endl;
+temp = n1;
+n1= n2;
+n2 = temp;
+cout<<"After swap int n1 = "<<n1<<" int n2 = "<<n2<<endl;
+} 
